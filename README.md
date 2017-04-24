e3b59e78255a6e9c779aade68f41b1ba6342b5d2
Swift <theycallmeswift@gmail.com>
2017-04-23 23:47:15 -0400

    Hello World!

---
+++ README.md
@@ -0,0 +1,43 @@
+# MLH Localhost: Hacking with Amazon Alexa
+
+This repositiory contains the sample code from the [MLH Localhost: Hacking
+with Amazon Alexa][1] workshop.
+
+## Sample Code / Downloads
+
+ - AWS Lambda Function - [Source][2] / [Lambda Zip][3]
+ - Alexa Intent Schema - [Source][4]
+ - Alexa Sample Utterances - [Source][5]
+
+## License
+
+Unless otherwise stated, the code in this repo is released under the MIT
+License.
+
+```
+Copyright (c) 2017 Major League Hacking, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE
+```
+
+[1]: https://localhost.mlh.io/activities/hacking-with-amazon-alexa/
+[2]: src/
+[3]: https://github.com/MLH/mlh-localhost-hacking-with-alexa/raw/master/downloads/lambda-function.zip
+[4]: SpeechAssets/IntentSchema.json
+[5]: SpeechAssets/SampleUtterances.txt
---
+++ src/index.js
@@ -0,0 +1,48 @@
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills
+ * nodejs skill development kit.  It's intended to be used at an MLH Localhost
+ * Workshop.
+ *
+ * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
+ * as testing instructions are located at https://github.com/mlh/mlh-localhost-alexa
+ **/
+
+'use strict';
+
+// TODO: replace with facts about yourself
+const FACTS = [
+  "Major League Hacking is commonly called M, L, H.",
+  "Major League Hacking's mission is to empower hackers.",
+  "Over 65,000 student hackers participated in Major League Hacking hackathons in 2016.",
+  "Major League Hacking was founded in 2013 by Mike Swift and Jon Gottfried.",
+  "Hackers created over 12,000 projects at Major League Hacking hackathons in 2016.",
+  "Over 200 universities and high schools around the world hosted Major League Hacking hackathons in 2016.",
+  "Major League Hacking is headquartered in New York City."
+];
+
+var handlers = {
+  'LaunchRequest': function () { this.emit('GetFact'); },
+  'GetNewFactIntent': function () { this.emit('GetFact'); },
+  'GetFact': function() {
+    // Randomly select a fact from the array
+    const factIndex = Math.floor(Math.random() * FACTS.length);
+    const randomFact = FACTS[factIndex];
+
+    // Create speech output
+    const speechOutput = "Here's your fact: " + randomFact;
+    this.emit(':tellWithCard', speechOutput, "Major League Hacking (MLH) Facts", randomFact);
+  }
+};
+
+// This is the function that AWS Lambda calls every time Alexa uses your skill.
+exports.handler = function(event, context, callback) {
+  // Include the AWS Alexa Library.
+  const Alexa = require("alexa-sdk");
+
+  // Create an instance of the Alexa library and pass it the requested command.
+  var alexa = Alexa.handler(event, context);
+
+  // Give our Alexa instance instructions for handling commands and execute the request.
+  alexa.registerHandlers(handlers);
+  alexa.execute();
+};
